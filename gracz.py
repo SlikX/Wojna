092e25521f5f994f47bcc377db901826027c0e55
SlikX <rafalkarwowski2017@gmail.com>
2022-11-01 00:22:16 +0100

    Commit WOJNA

---
+++ gracz.py
@@ -0,0 +1,13 @@
+from talia import Talia
+
+class Gracz:
+    imie: str
+    karty: list
+    # ile_kart: int
+
+    def __init__(self, imie, karty):
+        self.imie = imie
+        self.karty = karty
+        # self.ile_kart = ile_kart
+        # self.akt_karta = akt_karta
+
---
+++ karta.py
@@ -0,0 +1,8 @@
+import random
+from talia import Talia
+
+class Karta:
+    karta: Talia()
+    #
+    # def __init__(self):
+    #     self.karta = Talia().
